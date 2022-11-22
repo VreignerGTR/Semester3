5e8bdb533298773575681d8270d356d546b19fc5
VreignerOG <vreignertheog@gmail.com>
2022-11-22 13:57:29 +0700

    Session

---
+++ bahasa_pemograman/latihan/latihan1.py
@@ -0,0 +1,6 @@
+import calendar
+
+yy = 2022
+mm = 11
+
+print(calendar.month(yy, mm))
---
+++ bahasa_pemograman/latihan/latihan3.py
@@ -0,0 +1,4 @@
+num = 7
+num = int(input("Multiplication table of: "))
+for i in range(1, 11):
+    print(num, 'x', i, '=', num*i)
